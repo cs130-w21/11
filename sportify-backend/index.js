01af72b70ce283616bcf901f1dc3b9ddafcb8342
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2021-01-22 16:43:33 -0800

    successful postgres connection

--- sportify-backend/index.js
+++ sportify-backend/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const path = require("path");
+const pg = require('pg')
 
 const app = express();
 const port = process.env.PORT || "8000";
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to Sportify");
 });
 
 const { Sequelize } = require('sequelize')
-db_url = process.env.DB_CONNNECTION_URL || 'postgres://ahkzddesngriuf:e8ec8427f0f51ce1503d59b00f00ed228ecc33945bd9b1b50f5d0a7035bac42d@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d5vvatptuuuv2b'
-const sequelize = new Sequelize(db_url, {})
+// db_url = process.env.DB_CONNNECTION_URL || 'postgres://ahkzddesngriuf:e8ec8427f0f51ce1503d59b00f00ed228ecc33945bd9b1b50f5d0a7035bac42d@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d5vvatptuuuv2b'
+
+const sequelize = new Sequelize('d5vvatptuuuv2b', 'ahkzddesngriuf', 'e8ec8427f0f51ce1503d59b00f00ed228ecc33945bd9b1b50f5d0a7035bac42d', {
+    host: 'ec2-50-19-247-157.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 async function run() {
     try {
@@ -23,16 +39,11 @@ async function run() {
 
 run().catch(error => console.log(error.stack));
 
+const modelDefiners = [
+    require('./models/user'),
+    require('./models/game')
+]
 
-// const modelDefiners = [
-//     require('./models/user'),
-//     require('./models/game')
-// ]
-
-// for (const modelDefiner of modelDefiners) {
-//     modelDefiner(sequelize);
-// }
-
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
-});
+for (const modelDefiner of modelDefiners) {
+    modelDefiner(sequelize);
+}
