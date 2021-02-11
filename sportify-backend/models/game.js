43eee88c90accffd4812507f90adf8527cb34741
Rishab Sukumar <rishab.sukumar@chargepoint.com>
2021-02-11 21:05:05 +0400

    Modified game fields to be required. Modified getGames endpoint to include filter parameters. Added updateGame endpoint.

--- sportify-backend/models/game.js
+++ sportify-backend/models/game.js
@@ -3,15 +3,20 @@ const { DataTypes } = require('sequelize')
 module.exports = (sequelize) => {
     sequelize.define('game', {
         sport: {
+            allowNull: false,
             type: DataTypes.INTEGER
         },
+        // TODO: Modify to use coordinates
         location: {
+            allowNull: false,
             type: DataTypes.STRING
         },
         max_group_size: {
+            allowNull: false,
             type: DataTypes.INTEGER
         },
         skill_level: {
+            allowNull: false,
             type: DataTypes.INTEGER
         },
         comments: {
