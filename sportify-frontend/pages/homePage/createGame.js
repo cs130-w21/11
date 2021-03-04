98f1032b64b7479fc6e3e4668414d182c07a679b
Ishaan Pota <ishaan.pota@gmail.com>
2021-03-03 21:01:47 -0800

    Create games

--- sportify-frontend/pages/homePage/createGame.js
+++ sportify-frontend/pages/homePage/createGame.js
@@ -1,4 +1,4 @@
-import React, {component, useState, useEffect} from 'react'
+import React, { component, useState, useEffect } from 'react'
 import Head from 'next/head';
 import Link from 'next/link';
 import Container from 'react-bootstrap/Container';
@@ -11,37 +11,52 @@ import Geocode from 'react-geocode'
 
 const CreateGame = (props) => {
 
-	useEffect(()=>{
-        Geocode.setApiKey("AIzaSyDqs8TqTIsIx3xTuD1NEY3hXxmSciVrWZE");
-        Geocode.setLanguage("en");
+	useEffect(() => {
+		Geocode.setApiKey("AIzaSyDqs8TqTIsIx3xTuD1NEY3hXxmSciVrWZE");
+		Geocode.setLanguage("en");
+		const loggedInUser = localStorage.getItem("user-id");
+		if (loggedInUser) {
+			const foundUser = JSON.parse(loggedInUser);
+			setUser(foundUser);
 
-    }, []);
+		}
+		const loggedInUserName = localStorage.getItem("username");
+		if (loggedInUserName) {
+			const foundUserName = (loggedInUserName);
+			setUsername2(foundUserName);
+
+		}
+
+	}, []);
 
 	//const geoCoder=Maps.newGeocoder();
 
-	const [sport, setSport]=useState('');
+	const [sport, setSport] = useState('');
 	//const [name, setName]=useState('');
-	const [description, setDescription]=useState('');
-	const [numberOfPeopleIncludingYou, setNumberOfPeopleIncludingYou]=useState(1);
-	const [minSkillLevel, setMinSkillLevel]=useState(1);
+	const [description, setDescription] = useState('');
+	const [numberOfPeopleIncludingYou, setNumberOfPeopleIncludingYou] = useState(1);
+	const [minSkillLevel, setMinSkillLevel] = useState(1);
 	// const [maxSkillLevel, setMaxSkillLevel]=useState(10);
-	const [startTime, setStartTime]=useState(new Date());
+	const [startTime, setStartTime] = useState(new Date());
 
 	// let [latitude, setLatitude] = React.useState(0.1)
- 	//    let [longitude, setLongitude] = React.useState(0.1)
-    let [address, setAddress] = React.useState('')
+	//    let [longitude, setLongitude] = React.useState(0.1)
+	let [address, setAddress] = React.useState('')
+	const [foundUser, setUser] = useState("")
+	const [foundUserName, setUsername2] = useState("")
 	// const [endTime, setEndTime]=useState(new Date());
 	// const [gendersAllowed, setGendersAllowed]=useState("Both Men And Women");
 
-	const handleSubmit = (e)=>
-	{
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
 
 	};
 
 	return (
-				<div className="superContainer">
+		<div className="superContainer">
 
-				<style jsx global>{`
+			<style jsx global>{`
 					body {
 						background-color: pink;
 						align: auto;
@@ -50,54 +65,54 @@ const CreateGame = (props) => {
 					
 				`}</style>
 
-				<div className="navBar">
-					<ul>
-						<li>
-							<Link href='/userGames/userGames' passHref>
-								<div className="myGames">
-									<a>My Games</a>
-								</div>
-							</Link>
+			<div className="navBar">
+				<ul>
+					<li>
+						<Link href='/userGames/userGames' passHref>
+							<div className="myGames">
+								<a>My Games</a>
+							</div>
+						</Link>
 
-						</li>
+					</li>
 
-						<li>
-							<Link href='/fillOutProfile/viewOrEditProfile' passHref>
-								<div className="viewOrEditProfile">
-									<a>View/Edit my profile</a>
-								</div>
-							</Link>
-						</li>
+					<li>
+						<Link href='/fillOutProfile/viewOrEditProfile' passHref>
+							<div className="viewOrEditProfile">
+								<a>View/Edit my profile</a>
+							</div>
+						</Link>
+					</li>
 
-						<li>
-					        <Link href='/homePage/homePage' passHref>
-					        	<div className="homeDiv">
-									<a>Go home!</a>
-								</div>
-							</Link>
-						</li>
+					<li>
+						<Link href='/homePage/homePage' passHref>
+							<div className="homeDiv">
+								<a>Go home!</a>
+							</div>
+						</Link>
+					</li>
 
-						<li>
-					        <Link href='/frontpage/Login' passHref>
-					        	<div className="logoutDiv">
-									<a>Logout!</a>
-								</div>
-							</Link>
-						</li>
+					<li>
+						<Link href='/frontpage/Login' passHref>
+							<div className="logoutDiv">
+								<a>Logout!</a>
+							</div>
+						</Link>
+					</li>
 
-					</ul>
-				</div>
+				</ul>
+			</div>
 
-				<br/>
-				
-				<form onSubmit={(e)=>handleSubmit(e)}>
-					<label>
-			          Enter Sport:
-			          <input type="text" value={sport} onChange={(e)=>setSport(e.target.value)} />
-			        </label>
-			        <br />
+			<br />
 
-			        { /*
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<label>
+					Enter Sport:
+			          <input type="text" value={sport} onChange={(e) => setSport(e.target.value)} />
+				</label>
+				<br />
+
+				{ /*
 			        <label>
 			          Enter event name:
 			          <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
@@ -106,35 +121,35 @@ const CreateGame = (props) => {
 
 			    	*/ }
 
-			    	<label>
-			          Enter address/location:
-			          <input type="textArea" value={address} 
-			          onChange={ (e)=>setAddress(e.target.value) } />
-			        </label>
-			        <br />
+				<label>
+					Enter address/location:
+			          <input type="textArea" value={address}
+						onChange={(e) => setAddress(e.target.value)} />
+				</label>
+				<br />
 
 
-			        <label>
-			          Enter description:
-			          <input type="textArea" value={description} onChange={ (e)=>setDescription(e.target.value) } />
-			        </label>
-			        <br />
+				<label>
+					Enter description:
+			          <input type="textArea" value={description} onChange={(e) => setDescription(e.target.value)} />
+				</label>
+				<br />
 
-					<label>
-			          Min Skill Level:
+				<label>
+					Min Skill Level:
 			          <input type="number" value={minSkillLevel} min={1}
-			          onChange={(e)=>setMinSkillLevel(e.target.value)} />
-			        </label>
-			        <br />
+						onChange={(e) => setMinSkillLevel(e.target.value)} />
+				</label>
+				<br />
 
-			        <label>
-			          Number of people in the game including you (creator):
+				<label>
+					Number of people in the game including you (creator):
 			          <input type="number" value={numberOfPeopleIncludingYou} min={2}
-			          onChange={(e)=>setNumberOfPeopleIncludingYou(e.target.value)} />
-			        </label>
-			        <br />
+						onChange={(e) => setNumberOfPeopleIncludingYou(e.target.value)} />
+				</label>
+				<br />
 
-			        { /*
+				{ /*
 			        <label>
 			          Max Skill Level:
 			          <input type="number" value={maxSkillLevel} min={minSkillLevel} max={10}
@@ -144,15 +159,15 @@ const CreateGame = (props) => {
 
 			    	*/ }
 
-			          
-			        <label>
-			          	Starting time:
-			          	<input type="datetime-local"  value={startTime} onChange={(e)=>setStartTime(e.target.value)}/>
-			        </label>
-			        <br />
+
+				<label>
+					Starting time:
+			          	<input type="datetime-local" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+				</label>
+				<br />
 
 
-			        {/*   
+				{/*   
 			        <label>
 			          	Ending time:
 			          	<input type="datetime-local"  value={endTime} onChange={(e)=>setEndTime(e.target.value)}/>
@@ -160,7 +175,7 @@ const CreateGame = (props) => {
 			        <br />
 			        */}
 
-			        {/*   
+				{/*   
 			        <label>
 						Gender
 				        <select value={gendersAllowed} onChange={(e)=>setGendersAllowed(e.target.value)}>
@@ -173,7 +188,7 @@ const CreateGame = (props) => {
 					*/}
 
 
-			        { /* 
+				{ /* 
 					
 			        <button onClick={
 			        	(e) => {
@@ -185,36 +200,86 @@ const CreateGame = (props) => {
 			        </button>
 
 			        */}
-			        
-
-			        <Link href='/homePage/homePage' passHref>
-							<button> <a>Cancel creation of game! </a> </button>
-					</Link>
 
 
-			        <Link href='/homePage/homePage' passHref>
-							<button onClick={(e)=>{
-
-								// Get latitude & longitude from address.
-								Geocode.fromAddress(address).then(
-								  (response) => {
-								    const { lat, lng } = response.results[0].geometry.location;
-								    console.log(lat, lng);
-
-								  },
-								  (error) => {
-								    console.error(error);
-								  }
-								);
-
-							}}> <a>Submit creation of game! </a> </button>
-					</Link>
-		      	</form>
-	      	
-	      	</div>
+				<Link href='/homePage/homePage' passHref>
+					<button> <a>Cancel creation of game! </a> </button>
+				</Link>
 
 
-		);
+				<Link href='/homePage/homePage' passHref>
+					<button onClick={(e) => {
+						var lat = -76
+						var long = -148
+
+						// Get latitude & longitude from address.
+						Geocode.fromAddress(address).then(
+							(response) => {
+								lat, long = response.results[0].geometry.location;
+								console.log(lat, long);
+
+							},
+							(error) => {
+								console.error(error);
+							}
+						)
+						var currSport = 0
+						switch (sport) {
+							case "Basketball":
+								currSport = 1
+								break;
+							case "Tennis":
+								currSport = 2
+								break
+							case "Soccer":
+								currSport = 3
+								break
+							case "Badminton":
+								currSport = 4
+								break
+
+						}
+						console.log("wtf is going on")
+
+
+
+						const reactData = { "latitude": lat, "longitude": long, "dateString": startTime, "skill_level": minSkillLevel, "max_group_size": numberOfPeopleIncludingYou, "comments": description, "sport": currSport, "user": foundUser }
+
+
+
+						fetch('http://localhost:8000/games/createGame', {
+							//mode: "no-cors",
+							method: "POST",
+							headers: {
+								'Content-type': 'application/json',
+								//"Access-Control-Allow-Origin": '*'
+							},
+							body: JSON.stringify(reactData)
+						})
+							.then(response => response.json())
+							.then(json => {
+								if (json.message === "Signin successful") {
+									console.log("Result Here")
+									console.log(json)
+									Router.push("../homePage/homePage")
+								}
+								else {
+									console.log("here" + json)
+									console.log(json.message)
+								}
+
+
+
+							})
+
+					}}> <a>Submit creation of game! </a> </button>
+				</Link>
+			</form>
+
+		</div>
+
+
+	);
 
 };
 
